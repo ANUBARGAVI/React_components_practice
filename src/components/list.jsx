6fe884349234bd927c2216d3ce682dedf2c4483b
Anubargavi <anubargavi1119@gmail.com>
2024-07-20 14:49:28 +0530

    react

---
+++ src/components/list.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function List(){
+    return <ul>
+        <li>Chicken Rice</li>
+      <li>Veg Biriyani</li>
+      <li>Noodles</li>
+    </ul>
+}
+
+export default List;
